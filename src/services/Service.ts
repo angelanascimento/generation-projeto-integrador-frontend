4114aa2594362f93dd036718aa4db241ced00e53
Lilian Almeida <lili.l2002@hotmail.com>
2023-11-13 15:18:53 -0300

    Auth, Home e Login atualizados

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://comercio-com-causa.onrender.com/'
+    baseURL: 'https://comercio-com-causa.onrender.com'
 })
 
 export const login = async (url: string, dados: Object, setDados: Function) => {
