baf70e9d7f1b783f199d5ab51f9aa389ef139a8c
Avontss <jorgemchan@gmail.com>
2023-11-18 19:31:07 -0300

    Implementação da função deletar categoria

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -26,3 +26,6 @@ export const atualizar = async (url: string, dados: Object, setDados: Function, 
     const resposta = await api.put(url, dados, header)
     setDados(resposta.data)
 }
+export const deletar = async (url: string, header: Object) => {
+    await api.delete(url, header)
+}
