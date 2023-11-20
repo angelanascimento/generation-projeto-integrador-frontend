4e7cf2e3ea90d41268c0bc21ebb667a58f9e469a
Vinicius Bastos <vclemente2@hotmail.com>
2023-11-20 01:01:58 -0300

    Alteração dos estilos da página login e cadastro.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       backgroundColor: {
-        'cor01': '#385937'
+        cor01: "#385937"
+      },
+      colors: {
+        "cor-destaque": "#2fbf2a",
+        "cor-primaria": "#385937",
+        "cor-secundaria": "#2a332a",
+        "dark-grey": "#333333",
+        "intermediate-grey": "#7a7a7a",
+        "border-grey": "#bcbcbc",
+        "light-grey": "#dedede",
+        white: "#f7f7f7",
+        green: "#3fa33d",
+        "app-red": "#e94b35"
       }
-    },
+    }
   },
-  plugins: [],
-}
-
+  plugins: []
+};
