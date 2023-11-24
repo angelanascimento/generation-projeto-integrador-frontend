67acb51dcd0892af24b1d67c178682f5af7af8b6
Angela <commercial.araujo@gmail.com>
2023-11-24 11:01:05 -0300

    alteração na rota das imagens e implementação da lógica do Vinicius para página de cadastro

--- src/components/photos/PhotosAbout.tsx
+++ src/components/photos/PhotosAbout.tsx
@@ -3,7 +3,7 @@ import Image02 from "../../assets/img/photos-about/photo-02.jpg"
 import Image03 from "../../assets/img/photos-about/photo-03.jpg"
 import Image04 from "../../assets/img/photos-about/photo-04.jpg"
 import Image05 from "../../assets/img/photos-about/photo-05.jpg"
-import Image06 from "../../assets/img/photos-about/photo-06.jpg"
+import Image07 from "../../assets/img/photos-about/photo-07.jpg"
 
 
 function PhotosAbout() {
@@ -20,7 +20,7 @@ function PhotosAbout() {
                 <img className="rounded-xl col-start-1 col-span-3
                 row-start-5 row-span-2 h-full" src={Image03} alt="Fotografia posicionada lateralmente a vários caixotes de frutas empilhados em uma banca"/>
                 <img className="rounded-xl col-start-5 col-span-2
-                row-start-3 row-span-2 h-full" src={Image06} alt="Fotografia de dois caixotes de vegeitas, sendo o de baixo com pepinos e o de cima com tomates pequenos" />
+                row-start-3 row-span-2 h-full" src={Image07} alt="Fotografia de dois caixotes de vegeitas, sendo o de baixo com pepinos e o de cima com tomates pequenos" />
                 <img className="rounded-xl col-start-7 col-span-2
                 row-start-3 row-span-2 h-full" src={Image04} alt="Fotografia de uma melância pela metade no chão e duas criaças agachadas retirando um pedaço dela com suas respectivas colheres"/>                        
             </div>
